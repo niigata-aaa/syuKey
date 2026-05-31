cac758d1a0ba9ed8fb8667d3babd43bdff8d1d5c
sasa-max <kamisirotouya@gmail.com>
2026-05-31 17:34:51 +0900

    笹川：材料登録関連更新

--- MaterialManagementSystem/src/main/java/servlet/MaterialRegistConfirm.java
+++ MaterialManagementSystem/src/main/java/servlet/MaterialRegistConfirm.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.entity.MaterialBean;
 
 /**
  * Servlet implementation class MaterialRegistConfirm
@@ -13,14 +18,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/material-regist-confirm")
 public class MaterialRegistConfirm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MaterialRegistConfirm() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MaterialRegistConfirm() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -35,7 +40,30 @@ public class MaterialRegistConfirm extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession	session = request.getSession();
+		String user_id = (String)session.getAttribute("user_id");
+		request.setCharacterEncoding("UTF-8");
+		if(user_id != null) {
+			String material_name = request.getParameter("material_name");
+			String material_limit = request.getParameter("material_limit");
+			int material_amount = Integer.parseInt(request.getParameter("material_amount"));
+			
+			MaterialBean materialBean = new MaterialBean();
+			
+			materialBean.setAmount(material_amount);
+			materialBean.setLimits(material_limit);
+			materialBean.setMaterial_name(material_name);
+			materialBean.setUser_id(user_id);
+
+			request.setAttribute("materialBean", materialBean);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("material-regist-confirm.jsp");
+			rd.forward(request, response);
+
+		} else {
+			RequestDispatcher rd = request.getRequestDispatcher("login.html");
+			rd.forward(request, response);
+		}
 	}
 
 }
--- MaterialManagementSystem/src/main/java/servlet/MaterialRegistResultServlet.java
+++ MaterialManagementSystem/src/main/java/servlet/MaterialRegistResultServlet.java
@@ -1,11 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.dao.MaterialDAO;
+import model.entity.MaterialBean;
 
 /**
  * Servlet implementation class MaterialRegistResultServlet
@@ -35,7 +42,40 @@ public class MaterialRegistResultServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession();
+		request.setCharacterEncoding("UTF-8");
+		String material_name = request.getParameter("material_name");
+		int material_amount = Integer.parseInt(request.getParameter("material_amount"));
+		
+		String material_limit = request.getParameter("material_limit");
+		
+		int cnt = 0;
+		String url = "";
+		
+		
+		MaterialBean materialBean = new MaterialBean();
+		materialBean.setAmount(material_amount);
+		materialBean.setMaterial_name(material_name);
+		materialBean.setLimits(material_limit);
+		materialBean.setUser_id((String)session.getAttribute("user_id"));
+		
+		MaterialDAO materialDAO = new MaterialDAO();
+		
+		try {
+			cnt = materialDAO.regist(materialBean);
+		} catch(SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		if(cnt != 0) {
+			url = "material-regist-result.jsp";
+		} else {
+			url = "material-regist-failure.jsp";
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher(url);
+		rd.forward(request, response);
+
 	}
 
 }
